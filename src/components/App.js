37ab2a2a0878cff71ed3d4a613c31f919896293b
Cameron Abel <cameronabel@gmail.com>
2023-06-01 10:27:40 -0700

    convert to component directory structure

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App(){
+  return (
+    <React.Fragment>
+      <h1>Inventory</h1>
+      <h3>Placeholder product Code</h3>
+      <h3>Placeholder product Name</h3>
+      <p><em>Placeholder product quantity</em></p>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default App;
